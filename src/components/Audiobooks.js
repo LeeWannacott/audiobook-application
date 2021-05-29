9f759dbce4a520491bae4ffffa0c4e8413acfa20
Lee Wannacott <wannacottL@gmail.com>
2021-05-29 22:52:16 +1200

    Display images next to Audiobooks and on the Audiobooks page.

--- src/components/Audiobooks.js
+++ src/components/Audiobooks.js
@@ -7,7 +7,7 @@ import {
   View,
   ActivityIndicator,
 } from "react-native";
-import { ListItem, Image } from "react-native-elements";
+import { ListItem, Image, Avatar } from "react-native-elements";
 import AudiobookCard from "./AudiobookCard";
 import { useNavigation } from '@react-navigation/native';
 import React, { useState, useEffect } from "react";
@@ -15,13 +15,25 @@ import React, { useState, useEffect } from "react";
 function Audiobooks() {
   const [loadingAudioBooks, setLoadingAudioBooks] = useState(true);
   const [data, setData] = useState([]);
+  const [imageData, setImageData] = useState([]);
 
   useEffect(() => {
     fetch("https://librivox.org/api/feed/audiobooks/?&extended=1&format=json")
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
-      .finally(() => setLoadingAudioBooks(false));
+      .finally(() => {
+        // const bookCoverImages = [];
+        // setImageData(data.books)
+        // const bookData = Object.entries(imageData);
+        // bookData.forEach(([key, value]) => {
+          // console.log(key,value.url_zip_file)
+          // listRSSURLS.push(value.urls[0].url);
+        setLoadingAudioBooks(false)
+      },
+      );
+    
+
     // console.log(data.books)
 
     // fetch('https://librivox.org/rss/52')
@@ -37,22 +49,40 @@ function Audiobooks() {
   }, []);
   // keyExtractor = (item, index) => index.toString()
   // keyExtractor=({ id }, index) => id
+    const bookCoverURL = []
+  if (!loadingAudioBooks) {
+    const dataKeys = Object.entries(data.books);
+    var bookCoverImagePath;
+    dataKeys.forEach(([key, value]) => {
+      console.log(key,value.url_zip_file)
+      bookCoverImagePath = value.url_zip_file 
+      bookCoverImagePath = bookCoverImagePath.split("/");
+      bookCoverImagePath = bookCoverImagePath[bookCoverImagePath.length - 2];
+      bookCoverImagePath = `https://archive.org/services/get-item-image.php?identifier=${bookCoverImagePath}`;
+      bookCoverURL.push(bookCoverImagePath)
+  });
+  }
   const navigation = useNavigation();
   const keyExtractor = (item, index) => index.toString();
-  const renderItem = ({ item }) => (
+  const renderItem = ({ item, index}) => (
 
     <ListItem bottomDivider>
+
+    <Avatar 
+    source={{uri: bookCoverURL[index]}}
+    style={{ width: 150, height: 150 }}
+     />
       <ListItem.Content>
         <ListItem.Title>{item.title}</ListItem.Title>
         <ListItem.Subtitle>
           Author: {item.authors[0].first_name} {item.authors[0].last_name},
           Lived: {item.authors[0].dob} - {item.authors[0].dod}
+          Genre {item.genres[0].name}
         </ListItem.Subtitle>
-        <ListItem.Subtitle>Listen: {item.url_rss}</ListItem.Subtitle>
       </ListItem.Content>
       <ListItem.Chevron />
       <Button
-        onPress={() => navigation.navigate("Audio",[item.url_rss,item.id])}
+        onPress={() => navigation.navigate("Audio",[item.url_rss, item.id, item.url_zip_file])}
         title="Listen"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
@@ -61,6 +91,7 @@ function Audiobooks() {
   );
 
   if (!loadingAudioBooks) {
+    // console.log(data.books[1].url_zip_file)
   return (
     <View>
       <FlatList
--- src/screens/Audiotracks.js
+++ src/screens/Audiotracks.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, SafeAreaView } from "react-native";
-import { ListItem, Image, Slider } from "react-native-elements";
+import { ListItem, Image ,Tile} from "react-native-elements";
 import * as rssParser from "react-native-rss-parser";
 // import Sound from 'react-native-sound';
 import { Audio } from "expo-av";
+import Slider from "@react-native-community/slider";
 
 import {
   FlatList,
@@ -18,12 +19,29 @@ function Audiotracks(props) {
   const [loading, setLoading] = useState(true);
   const [loading2, setLoading2] = useState(true);
   const [data, setData] = useState([]);
-  const [data2, setData2] = useState([]);
-  const [data3, setData3] = useState([]);
+  const [AudioBookData, setAudioBookData] = useState([]);
+  const [AudioBookDescription, setAudioBookDescription] = useState([]);
   const [sound, setSound] = React.useState();
+  const [imageURL, setImageURL] = React.useState();
   const [isPlaying, setisPlaying] = useState(false);
   const [sliderValue, setSliderValue] = useState(0);
-  const [AudioBooksRSSLinkToAudioTracks, AudioBookId] = props.route.params;
+  const [
+    AudioBooksRSSLinkToAudioTracks,
+    AudioBookId,
+    urlZipFile,
+  ] = props.route.params;
+
+  useEffect(() => {
+    console.log(urlZipFile, "test");
+    if (urlZipFile.length !== 0) {
+      var bookCoverImagePath = urlZipFile;
+      bookCoverImagePath = bookCoverImagePath.split("/");
+      bookCoverImagePath = bookCoverImagePath[bookCoverImagePath.length - 2];
+      // console.log(bookCoverImagePath, "test1");
+      bookCoverImagePath = `https://archive.org/services/get-item-image.php?identifier=${bookCoverImagePath}`;
+      setImageURL(bookCoverImagePath);
+    }
+  }, []);
 
   useEffect(() => {
     fetch(AudioBooksRSSLinkToAudioTracks)
@@ -31,7 +49,7 @@ function Audiotracks(props) {
       .then((responseData) => rssParser.parse(responseData))
       .then((rss) => {
         setData(rss.items);
-        setData3(rss);
+        setAudioBookDescription(rss);
       })
       .catch((error) => console.error(error))
       .finally(() => {
@@ -49,18 +67,21 @@ function Audiotracks(props) {
       `https://librivox.org/api/feed/audiobooks/?id=${AudioBookId}&extended=1&format=json`
     )
       .then((response) => response.json())
-      .then((json) => setData2(json.books))
+      .then((json) => setAudioBookData(json.books))
       .catch((error) => console.error(error))
       .finally(() => setLoading2(false));
   }, []);
 
   async function playSound(itemURL) {
     console.log("Loading Sound");
+    await Audio.setAudioModeAsync({
+      staysActiveInBackground: true,
+    });
     const { sound } = await Audio.Sound.createAsync({ uri: itemURL });
     setSound(sound);
-    
+
     setisPlaying(true);
-      
+
     console.log("Playing Sound");
     await sound.playAsync();
   }
@@ -68,26 +89,30 @@ function Audiotracks(props) {
   function changeSliderValue() {
     if (isPlaying) {
       // setSliderValue(0.5);
-    setSliderValue(0.4)
+      setSliderValue(0.4);
     }
   }
 
   React.useEffect(() => {
     return sound
       ? () => {
-
-    // setSliderValue(0.4)
+          // setSliderValue(0.4)
           console.log("Unloading Sound");
           sound.unloadAsync();
         }
       : undefined;
   }, [sound]);
-
+// console.log(data)
   const listRSSURLS = [];
   const rssURLS = Object.entries(data);
   rssURLS.forEach(([key, value]) => {
     listRSSURLS.push(value.enclosures[0].url);
+    // console.log(value)
   });
+
+  // const imageData = Object.entries(AudioBookData);
+  // console.log(imageData[0][1].url_iarchive);
+
   const keyExtractor = (item, index) => index.toString();
   const renderItem = ({ item, index }) => (
     <View>
@@ -113,20 +138,33 @@ function Audiotracks(props) {
       </ListItem>
     </View>
   );
+
   if (!loading && !loading2) {
+    const imageData = Object.entries(AudioBookData);
+    // console.log(imageData[0][1].url_iarchive);
+    let AudioBooksCoverImage = imageData[0][1].url_iarchive;
+    AudioBooksCoverImage = AudioBooksCoverImage.substr(
+      AudioBooksCoverImage.lastIndexOf("/") + 1
+    );
+    // console.log(AudioBooksCoverImage);
     const getHeader = () => {
       return (
         <View style={styles.bookHeader}>
-          <Text style={styles.bookTitle}> {data2[0].title}</Text>
+          <Text style={styles.bookTitle}> {AudioBookData[0].title}</Text>
+<Image
+  source={{ uri: imageURL }}
+  style={{ width: 200, height: 200 }}
+/>
           <Text style={styles.bookDescription}>
             {" "}
-            By {data2[0].authors[0].first_name} {data2[0].authors[0].last_name}
+            By {AudioBookData[0].authors[0].first_name}{" "}
+            {AudioBookData[0].authors[0].last_name}
           </Text>
           <Text style={styles.bookDescription}>
             {" "}
-            Description: {data3.description}
+            Description: {AudioBookDescription.description}
           </Text>
-          <Text> Total time: {data2[0].totaltime} </Text>
+          <Text> Total time: {AudioBookData[0].totaltime} </Text>
         </View>
       );
     };
@@ -134,21 +172,24 @@ function Audiotracks(props) {
       <View style={styles.container}>
         <View style={styles.listItemHeaderStyle}>
           <FlatList
-            data={data2[0].sections}
+            data={AudioBookData[0].sections}
             keyExtractor={keyExtractor}
             renderItem={renderItem}
             ListHeaderComponent={getHeader}
           />
         </View>
         <View style={styles.bottom}>
-      <Slider 
-      value={sliderValue}
-      onValueChange={(sliderValue) => {setSliderValue(sliderValue)}}
-      step={0.1}
-      allowTouchTrack={true}
-      minimumValue={0}
-      maximumValue={100}
-      />
+          <Slider
+            style={{ width: 320, height: 40 }}
+            value={sliderValue}
+            onValueChange={(sliderValue) => {
+              setSliderValue(sliderValue);
+            }}
+            step={0.1}
+            allowTouchTrack={true}
+            minimumValue={0}
+            maximumValue={100}
+          />
           <Text>{sliderValue} world</Text>
         </View>
       </View>
@@ -165,6 +206,7 @@ function Audiotracks(props) {
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 0.9,
@@ -175,7 +217,7 @@ const styles = StyleSheet.create({
     alignContent: "flex-end",
     // top:100,
     padding: 10,
-    top:-100
+    top: -100,
   },
   listItemHeaderStyle: {
     fontSize: 20,
