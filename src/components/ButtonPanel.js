027dc0af404b358828d98c71250d05720e8830fc
Lee Wannacott <wannacottL@gmail.com>
2021-10-22 23:32:57 +1300

    Add fourth user settings button to buttonpanel and refactor buttonpanelto be more descriptive.

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,62 +1,64 @@
-import React,{useState} from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Button, ButtonGroup } from "react-native-elements";
+import React, { useState } from "react";
+import { ButtonGroup } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-function ButtonPanel(props) {
-const [selectedIndex, updateIndex] = useState(2);
-    const component1 = () => (
-      <Text>
-        <Icon
-          name="search"
-          size={50}
-          color={selectedIndex === 0 ? "white" : "#000"}
-        />{" "}
-        Explore
-      </Text>
-    );
-    const component2 = () => (
-      <Text>
-        <Icon
-          name="star"
-          size={50}
-          color={selectedIndex === 1 ? "white" : "#000"}
-        />{" "}
-        Starred
-      </Text>
-    );
-    const component3 = () => (
-      <Text>
-        <Icon
-          name="history"
-          size={50}
-          color={selectedIndex === 2 ? "white" : "#000"}
-        />{" "}
-        History
-      </Text>
-    );
-    const buttons = [
-      { element: component1 },
-      { element: component2 },
-      { element: component3 },
-    ];
+function ButtonPanel() {
+  const [selectedIndex, updateIndex] = useState(0);
 
-    return (
-      <>
-        <ButtonGroup
-          onPress={updateIndex}
-          selectedIndex={selectedIndex}
-          buttons={buttons}
-          containerStyle={{
-            height: 71,
-            width: 340,
-            top: 24,
-            left: -10,
-            borderRadius: 0,
-          }}
-        />
-      </>
-    );
-  
+  const Explore = () => (
+    <Icon
+      name="search"
+      size={50}
+      color={selectedIndex === 0 ? "white" : "#000"}
+    />
+  );
+
+  const Favourited = () => (
+    <Icon
+      name="star"
+      size={50}
+      color={selectedIndex === 1 ? "white" : "#000"}
+    />
+  );
+
+  const History = () => (
+    <Icon
+      name="history"
+      size={50}
+      color={selectedIndex === 2 ? "white" : "#000"}
+    />
+  );
+
+  const Settings = () => (
+    <Icon name="cog"
+    size={50}
+    color={selectedIndex === 3 ? "white" : "#000"}
+    />
+  );
+
+  const buttons = [
+    { element: Explore },
+    { element: Favourited },
+    { element: History },
+    { element: Settings },
+  ];
+
+  return (
+    <>
+      <ButtonGroup
+        onPress={updateIndex}
+        selectedIndex={selectedIndex}
+        buttons={buttons}
+        containerStyle={{
+          height: 71,
+          width: 340,
+          top: 24,
+          left: -10,
+          borderRadius: 0,
+        }}
+      />
+    </>
+  );
 }
+
 export default ButtonPanel;
